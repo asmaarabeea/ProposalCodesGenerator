821e86b340adf4c02414cd62c73f8f0028eb638b
asmaa_rabeea22 <asmaa.rabie@jobzella.com>
2018-10-24 10:08:24 +0200

    add comment for technical_approval

--- app/Http/Services/ProposalCodeService.php
+++ app/Http/Services/ProposalCodeService.php
@@ -15,11 +15,12 @@ class ProposalCodeService
     {
         $requested_data['sales_agent'] = $requested_data['user']->full_name;
         $requested_data ['user_id']    = $requested_data['user']->id;
-        $proposal_type                 = $this->getFirstSecChar($requested_data['proposal_type']);
-        $technical_approval            = $this->getFirstSecChar($requested_data['technical_approval']);
-        $sales_agent                   = $this->getFirstSecChar($requested_data['sales_agent']);
-        $client_source                 = $this->getFirstSecChar($requested_data['client_source']);
-        $requested_data ['code']       = $proposal_type . $technical_approval . '-' . $requested_data['proposal_number'] . '-' . $client_source . $sales_agent;
+
+        $proposal_type           = $this->getFirstSecChar($requested_data['proposal_type']);
+        $technical_approval      = $this->getFirstSecChar($requested_data['technical_approval']); //technical_approval comes from users where auth()->user()->role_id = 2
+        $sales_agent             = $this->getFirstSecChar($requested_data['sales_agent']);
+        $client_source           = $this->getFirstSecChar($requested_data['client_source']);
+        $requested_data ['code'] = $proposal_type . $technical_approval . '-' . $requested_data['proposal_number'] . '-' . $client_source . $sales_agent;
 
         return Code::create($requested_data);
     }
@@ -43,6 +44,7 @@ class ProposalCodeService
     {
         return Code::where('user_id', $user->id)->select('code')->get()->toArray();
     }
+
     /**
      * @param $string
      *
